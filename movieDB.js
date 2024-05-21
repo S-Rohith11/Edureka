d48f0abb32b2e82ba641f50c60b63a866e75f794
Rohith <82258311+S-Rohith11@users.noreply.github.com>
2024-05-21 22:21:19 +0530

    Delete movieDB.js

--- movieDB.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const movieSchema = new Schema({
-    _id: Number
-})
-
-module.exports = mongoose.model('movieData', movieSchema, 'movie');
